80150792cd9cf4229aae75769c35df4dd1d0c38e
H.P. Grahsl <grahslhp@gmail.com>
2023-05-09 16:52:13 +0200

    removing outdated app folder

--- apps/ng-poi-map/src/assets/env.template.js
+++
@@ -1,9 +0,0 @@
-(function (window) {
-  window["env"] = window["env"] || {};
-  window["env"]["mapTitle"] = "${MAP_TITLE}";
-  window["env"]["mapAttribution"] = "${MAP_ATTRIBUTION}";
-  window["env"]["mapUrl"] = "${MAP_URL}";
-  window["env"]["mapToken"] = "${MAP_TOKEN}";
-  window["env"]["gatewayApiUrl"] = "${GATEWAY_API_URL}";
-  window["env"]["websocketEndpoint"] = "${WEBSOCKET_ENDPOINT}";
-})(this);
